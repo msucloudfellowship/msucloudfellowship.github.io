a262c90c88e505a100dd56ed80b1ec604c71ec25
Pat Bills <billspat@users.noreply.github.com>
2022-11-18 15:00:41 -0500

    Deployed 1c855f7 with MkDocs version: 1.3.1

--- session_serverless/docker_cloud_build_demo/docker_build_jupyter_on_azure_demo.sh
+++ session_serverless/docker_cloud_build_demo/docker_build_jupyter_on_azure_demo.sh
@@ -13,34 +13,46 @@
 # - Dockerfile in the current directory
 
 # set the names of things for commands below
-# this
+# this is so we can easily edit this script with
+# a different Dockerfile and purpose
 #### PUT YOUR RESOURCE GROUP HERE
 AZRG=
+### PUT YOUR Azure or other unique ID here
+MYID=
+
+OURIMAGENAME=jupyter-nltk
 AZACI=aci-jupyterlab-$MYID
-AZACR=acrccf22$MYID
+AZACR=acr$MYID
+# to facility using multiple dockerfiles in this folder, use a variable
+# to indicate which dockerfile name we are using here
+DOCKERFILE=Dockerfile
 
 # create a container registry to hold the images
+# NOTE you can re-use this ACR for many images, so it's ok to if you get an error
+# becuase it already exists
+
 az acr create -n $AZACR -g $AZRG --sku Standard --admin-enabled true
 
 # the "admin-enabled" option is so we can use password to access this private registry, which is simpler
 # than using 
 # build the image using the acr
-az acr build -t jupyter-nltk:latest --file Dockerfile --registry $AZACR .
+# USE A DIFFERENT FILE NAME THAN "Dockerfile" if you are re-purposing this script
+az acr build -t $OURIMAGE:latest --file Dockerfile --registry $AZACR .
 
-# azure command to show 
+# azure command to get the admin password for the ACR we just created
+# the PW is needed by the next command to pull images from this private registry 
 export AZ_ACR_PW=$(az acr credential show --name $AZACR -g $AZRG  --output tsv  --query="passwords[0]|value")
 
 az container create --resource-group $AZRG --name $AZACI \
-  --image $AZACR.azurecr.io/jupyter-nltk:latest  \
-  --dns-name-label jupyter-nltk-$MYID --ports 8888 \
+  --image $AZACR.azurecr.io/$OURIMAGENAME:latest  \
+  --dns-name-label $OURIMAGENAME-$MYID --ports 8888 \
   --registry-username $AZACR  \
   --registry-password $AZ_ACR_PW
 
-# get the login information needed
+# # Jupyter specific : get the web address and token out of the logs
+# remove this if you use this script for other types of images
 fqdn=$(az container show -g $AZRG --name $AZACI --output tsv --query ipAddress.fqdn)
-
 echo "the Juypter server is running on  https://${fqdn}:8888"
 
-# get the token out of the logs
 echo "view the code below and copy/paste the token for logging into the jupyter server"
 az container logs --resource-group $AZRG --name $AZACI | grep token
