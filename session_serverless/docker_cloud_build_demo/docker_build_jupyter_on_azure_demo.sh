cfa1b87fba7d26c94b0a89cfb38fadf07b21e3ad
Pat Bills <billspat@users.noreply.github.com>
2022-12-02 00:21:34 -0500

    Deployed 5ff6abe with MkDocs version: 1.3.1

--- session_serverless/docker_cloud_build_demo/docker_build_jupyter_on_azure_demo.sh
+++ session_serverless/docker_cloud_build_demo/docker_build_jupyter_on_azure_demo.sh
@@ -1,4 +1,4 @@
-# container_run.sh
+# docker_build_jupyter_on_azure_demo.sh
 
 # build an image basd on the Jupyterlab Stacks and run a container using Azure
 # this script will run on Mac, Linux or the Windows subsystem for Linux (WSL)
@@ -16,14 +16,14 @@
 # this is so we can easily edit this script with
 # a different Dockerfile and purpose
 #### PUT YOUR RESOURCE GROUP HERE
-AZRG=
+AZRG=ccf22_billspat
 ### PUT YOUR Azure or other unique ID here
-MYID=
+MYID=billspat
 
 OURIMAGENAME=jupyter-nltk
 AZACI=aci-jupyterlab-$MYID
 AZACR=acr$MYID
-# to facility using multiple dockerfiles in this folder, use a variable
+# to facilitate using multiple dockerfiles in this folder, use a variable
 # to indicate which dockerfile name we are using here
 DOCKERFILE=Dockerfile
 
@@ -31,28 +31,44 @@ DOCKERFILE=Dockerfile
 # NOTE you can re-use this ACR for many images, so it's ok to if you get an error
 # becuase it already exists
 
+az_jupyter_create () {
 az acr create -n $AZACR -g $AZRG --sku Standard --admin-enabled true
 
 # the "admin-enabled" option is so we can use password to access this private registry, which is simpler
 # than using 
 # build the image using the acr
 # USE A DIFFERENT FILE NAME THAN "Dockerfile" if you are re-purposing this script
-az acr build -t $OURIMAGE:latest --file Dockerfile --registry $AZACR .
+az acr build -t ${OURIMAGENAME}:latest --file Dockerfile --registry $AZACR .
 
 # azure command to get the admin password for the ACR we just created
 # the PW is needed by the next command to pull images from this private registry 
 export AZ_ACR_PW=$(az acr credential show --name $AZACR -g $AZRG  --output tsv  --query="passwords[0]|value")
 
 az container create --resource-group $AZRG --name $AZACI \
-  --image $AZACR.azurecr.io/$OURIMAGENAME:latest  \
-  --dns-name-label $OURIMAGENAME-$MYID --ports 8888 \
+  --image $AZACR.azurecr.io/${OURIMAGENAME}:latest  \
+  --dns-name-label ${OURIMAGENAME}-${MYID} --ports 8888 \
   --registry-username $AZACR  \
   --registry-password $AZ_ACR_PW
 
-# # Jupyter specific : get the web address and token out of the logs
+## Jupyter specific : get the web address and security token out of the logs 
+## so you can log-in to the Jupyter Lab server. 
 # remove this if you use this script for other types of images
 fqdn=$(az container show -g $AZRG --name $AZACI --output tsv --query ipAddress.fqdn)
-echo "the Juypter server is running on  https://${fqdn}:8888"
+echo "the Juypter server is running on  http://${fqdn}:8888"
 
 echo "view the code below and copy/paste the token for logging into the jupyter server"
 az container logs --resource-group $AZRG --name $AZACI | grep token
+
+}
+
+## excercise for the reader:  how to create and mount cloud storage when creating the container instance 
+# in order to save scripts and data in common location accessible.  
+
+### clean up
+# this creates a function in your terminal to delete the resources just created
+# if you close the terminal the function is lost.  
+az_jupyter_cleanup () {
+  az container delete -g $AZRG --name $AZACI
+  az acr delete -n $AZACR -g $AZRG
+
+}
